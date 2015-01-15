293ffffaeb76f137153ddb67393b2a988f99afaf
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-01-15 22:24:20 +0800

    login

--- script/app.js
+++ script/app.js
@@ -18,3 +18,27 @@ window.fbAsyncInit = function() {
   js.src = '//connect.facebook.net/en_US/sdk.js';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+// Place following code after FB.init call.
+
+function onLogin(response) {
+  if ('connected' === response.status) {
+    FB.api('/me?fields=first_name', function(data) {
+      var welcomeBlock = document.getElementById('fb-welcome');
+      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+    });
+  }
+}
+
+FB.getLoginStatus(function(response) {
+  // Check login status on load, and if the user is
+  // already logged in, go directly to the welcome message.
+  if ('connected' === response.status) {
+    onLogin(response);
+  } else {
+    // Otherwise, show Login dialog first.
+    FB.login(function(response) {
+      onLogin(response);
+    }, {scope: 'user_friends, email'});
+  }
+});
